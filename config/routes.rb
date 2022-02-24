0c6a8ab2bca1ecb83b553ab7e35d615e7a5c5e05
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-24 15:25:45 +0100

    ✨feat(devise): ajout de devise

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
